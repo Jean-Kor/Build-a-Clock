3cd2721b778e7e9b38a15b2f8c35e73ecd40fb6b
Kenneth Cabel <21jeaday@waipahuh.k12.hi.us>
2020-01-13 09:33:37 -1000

    Fifth

--- clock.js
+++ clock.js
@@ -1,1 +1,8 @@
 var timeDisplay = clock.js.getElementById('time');
+
+function timechange() {
+	var dateString = new Date().toLocaleString("en-us", {timezone: "EST"});
+	var
+}
+
+setInterval ("timechange();",1000);
